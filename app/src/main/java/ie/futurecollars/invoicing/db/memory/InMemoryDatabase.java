c4ed7b6da1e61604c7959e50b15731dce03ba527
TeresaTao127 <taoleiluck2020@gmail.com>
2023-09-17 14:06:13 +0100

    lesson2 basic structure

---
+++ app/src/main/java/ie/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -0,0 +1,36 @@
+package ie.futurecollars.invoicing.db.memory;
+
+import ie.futurecollars.invoicing.db.Database;
+import ie.futurecollars.invoicing.model.Invoice;
+import java.util.List;
+import java.util.Optional;
+
+
+public class InMemoryDatabase implements Database {
+  @Override
+  public int save(Invoice invoice) {
+    return 0;
+  }
+
+  @Override
+  public Optional<Invoice> getById(int id) {
+    return Optional.empty();
+  }
+
+  @Override
+  public List<Invoice> getAll() {
+    return null;
+  }
+
+  @Override
+  public void update(int id, Invoice updatedInvoice) {
+
+  }
+
+  @Override
+  public void delete(int id) {
+
+  }
+}
+
+
