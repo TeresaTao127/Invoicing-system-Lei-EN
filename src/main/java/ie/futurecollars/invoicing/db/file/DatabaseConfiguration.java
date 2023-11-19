dff5cc1af3f8ca73157e5d3302c6796e96054d5d
TeresaTao127 <taoleiluck2020@gmail.com>
2023-11-19 19:20:57 +0000

    lesson 5 checkStyle

--- src/main/java/ie/futurecollars/invoicing/db/file/DatabaseConfiguration.java
+++ src/main/java/ie/futurecollars/invoicing/db/file/DatabaseConfiguration.java
@@ -1,16 +1,17 @@
 package ie.futurecollars.invoicing.db.file;
 
+import ie.futurecollars.invoicing.db.Database;
+import ie.futurecollars.invoicing.utils.FilesService;
+import ie.futurecollars.invoicing.utils.JsonService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ie.futurecollars.invoicing.db.Database;
-import ie.futurecollars.invoicing.utils.FilesService;
-import ie.futurecollars.invoicing.utils.JsonService;
 
 @Configuration
 public class DatabaseConfiguration {
+
   private static final String DATABASE_LOCATION = "db";
   private static final String ID_FILE_NAME = "id.txt";
   private static final String INVOICES_FILE_NAME = "invoices.txt";
