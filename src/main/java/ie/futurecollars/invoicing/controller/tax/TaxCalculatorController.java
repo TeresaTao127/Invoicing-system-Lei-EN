363b7ed49d557e58a240c8ec89d826c8a2a8e088
TeresaTao127 <taoleiluck2020@gmail.com>
2023-12-05 22:05:51 +0000

    lesson 10 Tax calculator extending income tax

--- src/main/java/ie/futurecollars/invoicing/controller/tax/TaxCalculatorController.java
+++ src/main/java/ie/futurecollars/invoicing/controller/tax/TaxCalculatorController.java
@@ -1,8 +1,10 @@
 package ie.futurecollars.invoicing.controller.tax;
 
+import ie.futurecollars.invoicing.model.Company;
 import ie.futurecollars.invoicing.service.TaxCalculatorResult;
 import ie.futurecollars.invoicing.service.TaxCalculatorService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +14,7 @@ public class TaxCalculatorController implements TaxCalculatorApi {
   private final TaxCalculatorService taxService;
 
   @Override
-  public TaxCalculatorResult calculateTaxes(String taxIdentificationNumber) {
-    return taxService.calculateTaxes(taxIdentificationNumber);
+  public TaxCalculatorResult calculateTaxes(@RequestBody Company company) {
+    return taxService.calculateTaxes(company);
   }
 }
