66e64b62669ea30b6e43c1264cb6143c7fdd0b8f
TeresaTao127 <taoleiluck2020@gmail.com>
2023-12-10 22:00:51 +0000

    lesson 12 SQL database

--- src/main/java/ie/futurecollars/invoicing/db/DatabaseConfiguration.java
+++ src/main/java/ie/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -3,6 +3,7 @@ package ie.futurecollars.invoicing.db;
 import ie.futurecollars.invoicing.db.file.FileBasedDatabase;
 import ie.futurecollars.invoicing.db.file.IdProvider;
 import ie.futurecollars.invoicing.db.memory.InMemoryDatabase;
+import ie.futurecollars.invoicing.db.sql.SqlDatabase;
 import ie.futurecollars.invoicing.utils.FilesService;
 import ie.futurecollars.invoicing.utils.JsonService;
 import java.io.IOException;
@@ -13,12 +14,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Slf4j
 @Configuration
 public class DatabaseConfiguration {
 
   @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "file")
   public IdProvider idProvider(
       FilesService filesService,
       @Value("${invoicing-system.database.directory}") String databaseDirectory,
@@ -37,15 +40,20 @@ public class DatabaseConfiguration {
       @Value("${invoicing-system.database.directory}") String databaseDirectory,
       @Value("${invoicing-system.database.invoices.file}") String invoicesFile
   ) throws IOException {
-    log.debug("Creating in-file database");
     Path databaseFilePath = Files.createTempFile(databaseDirectory, invoicesFile);
     return new FileBasedDatabase(databaseFilePath, idProvider, filesService, jsonService);
   }
 
-  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "memory")
   @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "sql")
+  public Database sqlDatabase(JdbcTemplate jdbcTemplate) {
+    return new SqlDatabase(jdbcTemplate);
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "memory")
   public Database inMemoryDatabase() {
-    log.debug("Creating in-memory database");
     return new InMemoryDatabase();
   }
+
 }
