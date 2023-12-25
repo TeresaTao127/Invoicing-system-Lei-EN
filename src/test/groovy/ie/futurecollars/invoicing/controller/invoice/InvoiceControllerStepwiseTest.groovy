8ad6197d73b361b363b23736cb5f2d4b449d144b
TeresaTao127 <taoleiluck2020@gmail.com>
2023-12-25 13:22:56 +0000

    Lesson 14 mongo fix invoiveControllerIntegrationTest and stepwiseTest

--- src/test/groovy/ie/futurecollars/invoicing/controller/invoice/InvoiceControllerStepwiseTest.groovy
+++ src/test/groovy/ie/futurecollars/invoicing/controller/invoice/InvoiceControllerStepwiseTest.groovy
@@ -1,14 +1,16 @@
 package ie.futurecollars.invoicing.controller.invoice
 
-
+import com.mongodb.client.MongoDatabase
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import ie.futurecollars.invoicing.helpers.TestHelpers
 import ie.futurecollars.invoicing.model.Invoice
 import ie.futurecollars.invoicing.utils.JsonService
+import spock.lang.Requires
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
@@ -30,12 +32,22 @@ class InvoiceControllerStepwiseTest extends Specification {
     @Autowired
     private JsonService jsonService;
 
-    private Invoice originalInvoice = TestHelpers.invoice(1)
+    private final Invoice originalInvoice = TestHelpers.invoice(1)
 
-    private LocalDate updatedDate = LocalDate.of(2020, 02, 28)
+    private final LocalDate updatedDate = LocalDate.of(2020, 02, 28)
 
     @Shared
     private int invoiceId
+
+    @Autowired
+    private ApplicationContext context
+
+    @Requires({ System.getProperty('spring.profiles.active', 'memory').contains("mongo") })
+    def "database is dropped to ensure clean state"() {
+        expect:
+        MongoDatabase mongoDatabase = context.getBean(MongoDatabase)
+        mongoDatabase.drop()
+    }
 
     def "empty array is returned when no invoices were added"() {
         when:
